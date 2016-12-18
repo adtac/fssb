ddc6bad6dd12709d91b0399d71ba2a833de60bfb
Adhityaa C <c.adhityaa@gmail.com>
2016-12-18 16:06:31 +0530

    syscalls: Add LICENSE at the top

--- syscalls.h
+++ syscalls.h
@@ -1,3 +1,22 @@
+/**
+ * proxyfile.h - Proxy file operations.  Part of the FSSB project.
+ *
+ * Copyright (C) 2016 Adhityaa Chandrasekar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define SC_READ       0
 #define SC_WRITE      1
 #define SC_OPEN       2
